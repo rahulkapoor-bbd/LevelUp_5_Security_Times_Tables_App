a97dfa1856c5e772b6f8951eaaa8c6e9174bac4e
Kyle Pottinger <kylep@bbd.co.za>
2023-06-21 15:14:55 +0200

    fix(App.js): Removed ejs renderer for html

--- routes/personalscores.js
+++ routes/personalscores.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('personalscores.html', { title: 'Personal Score' });
+  res.sendFile('personalscores.html', { root: 'views' });
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('register.html', { title: 'Register' });
+  res.sendFile('register.html', { root: 'views' });
 });
 
 module.exports = router;
