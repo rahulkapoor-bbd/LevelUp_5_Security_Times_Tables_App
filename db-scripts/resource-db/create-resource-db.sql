237c5b6a3ef58c80cfc7b300a6d56fb5d9fcf514
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-06-22 17:42:49 +0200

    Added DB script for resource DB

---
+++ db-scripts/resource-db/create-resource-db.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE TimesTableApp;
+
+USE TimesTableApp;
+
+CREATE TABLE User(
+UserID int NOT NULL,
+DifficultyID int NOT NULL,
+Username varchar(64) NOT NULL UNIQUE,
+FirstName varchar(128) NOT NULL,
+PasswordHash varchar(256) NOT NULL,
+Salt varchar(64) NOT NULL,
+PRIMARY KEY (UserID)
+);
+
+CREATE TABLE Difficulty(
+DifficultyID int NOT NULL,
+DifficultyName varchar(20) NOT NULL UNIQUE,
+DifficultyDescription varchar(512) NOT NULL,
+PRIMARY KEY (DifficultyID)
+);
+
+CREATE TABLE Score(
+ScoreID int NOT NULL,
+UserID int NOT NULL,
+DifficultyID int NOT NULL,
+ScoreDate datetime NOT NULL,
+ScoreValue float NOT NULL,
+PRIMARY KEY (ScoreID)
+);
+
+ALTER TABLE User
+ADD FOREIGN KEY (DifficultyID)
+REFERENCES Difficulty(DifficultyID);
+
+ALTER TABLE Score
+ADD FOREIGN KEY (UserID)
+REFERENCES User(UserID);
+
+ALTER TABLE Score
+ADD FOREIGN KEY (DifficultyID)
+REFERENCES Difficulty(DifficultyID);
