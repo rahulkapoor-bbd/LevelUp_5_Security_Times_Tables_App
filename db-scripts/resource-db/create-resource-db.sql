cb7be90e3b5c6755179aad1ab428e111fa734c45
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-06-23 08:44:47 +0200

    Updated resource db script and added difficulties

--- db-scripts/resource-db/create-resource-db.sql
+++ db-scripts/resource-db/create-resource-db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE TimesTableApp;
 USE TimesTableApp;
 
 CREATE TABLE User(
-UserID int NOT NULL,
+UserID int NOT NULL AUTO_INCREMENT,
 DifficultyID int NOT NULL,
 Username varchar(64) NOT NULL UNIQUE,
 FirstName varchar(128) NOT NULL,
@@ -13,14 +13,14 @@ PRIMARY KEY (UserID)
 );
 
 CREATE TABLE Difficulty(
-DifficultyID int NOT NULL,
+DifficultyID int NOT NULL AUTO_INCREMENT,
 DifficultyName varchar(20) NOT NULL UNIQUE,
 DifficultyDescription varchar(512) NOT NULL,
 PRIMARY KEY (DifficultyID)
 );
 
 CREATE TABLE Score(
-ScoreID int NOT NULL,
+ScoreID int NOT NULL AUTO_INCREMENT,
 UserID int NOT NULL,
 DifficultyID int NOT NULL,
 ScoreDate datetime NOT NULL,
